8512f9e28ad698dfdf24fe470e67b56b8405745a
afamko <afam@iotpyramid.com>
2024-03-09 16:35:33 -0500

    a

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/deltonutsey/kaminari_prime
+# Install script for directory: /Users/AfamObioha/kaminari_prime
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/deltonutsey/kaminari_prime/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/AfamObioha/kaminari_prime/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
